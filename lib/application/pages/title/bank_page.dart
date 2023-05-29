8936ef1f307ffd1145d226cd3ddf38218beef33a
Paweł Wlazło <pawel@wlazlo.dev>
2023-05-29 19:05:08 +0200

    dodanie przycisku zmiany motywu

--- lib/application/pages/title/bank_page.dart
+++ lib/application/pages/title/bank_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_playground/application/core/services/theme_service.dart';
 import 'package:flutter_playground/application/pages/title/widgets/bottom_section.dart';
 import 'package:flutter_playground/application/pages/title/widgets/center_section.dart';
+import 'package:provider/provider.dart';
 
 class BankPage extends StatelessWidget {
   const BankPage({super.key});
@@ -12,6 +14,14 @@ class BankPage extends StatelessWidget {
         appBar: AppBar(
           title: const Text("Easy Bank"),
           titleTextStyle: themeData.textTheme.displaySmall,
+          actions: [
+            Switch(
+                value: Provider.of<ThemeService>(context).isDarkModeOn,
+                onChanged: (_) {
+                  Provider.of<ThemeService>(context, listen: false)
+                      .toggleTheme();
+                }),
+          ],
         ),
         body: const Center(
           child: Column(children: <Widget>[
