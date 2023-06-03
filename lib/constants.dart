fe1e9ab89f73fbe5013a201d4263e6bf7982a345
Paweł Wlazło <pawel@wlazlo.dev>
2023-06-03 19:29:28 +0200

    dodanie stałych wartości

---
+++ lib/constants.dart
@@ -0,0 +1,4 @@
+const ingLogo = 'assets/images/ing_logo.png';
+const aliorLogo = 'assets/images/alior_logo.png';
+const blik = 'assets/images/blik.png';
+const blikPhone = 'assets/images/blik_phone.png';
