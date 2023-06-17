f1e2fcac515d816fdb08f664cb7ce950a6b2a144
pawelwlazlo <pawel@wlazlo.dev>
2023-06-17 15:27:03 +0200

    dodanie bank transfer domain layer

--- lib/data/bank/datasources/bank_transfer_data_source.dart
+++ lib/data/bank/datasources/bank_transfer_data_source.dart
@@ -1,4 +1,5 @@
 import 'package:decimal/decimal.dart';
+import 'package:faker_dart/faker_dart.dart';
 
 import '../../../domain/bank_transfer/entities/bank_transfer.dart';
 
@@ -6,21 +7,25 @@ abstract class BankTransferDataSource {
   Stream<BankTransfer> getBankTransfers();
 }
 
-
 class BankTransferDataSourceImpl implements BankTransferDataSource {
   @override
   Stream<BankTransfer> getBankTransfers() {
+    final faker = Faker.instance;
     return Stream.fromIterable(
-      // generate 40 bank transfers with names in polish language
-      List.generate(40, (index) => BankTransfer(
-        amount: Decimal.parse('${index * 100.0}'),
-        transferDate: DateTime.now(),
-        accountNumber: 'PL 00 0000 0000 0000 0000 0000 0000',
-        title: 'Tytuł przelewu $index',
-        transferId: index,
-        accountingDate: DateTime.now(),
-        accountHolderName: 'Jan Kowalski', bankId: 1,//random bank id,
-      ))
-    );
+        // generate 40 bank transfers with names in polish language
+        List.generate(
+            40,
+            (index) => BankTransfer(
+                  amount: Decimal.parse('${index * 100.0}'),
+                  transferDate: DateTime.now(),
+                  accountNumber:
+                      'PL 00 ${faker.datatype.number(min: 1111, max: 9999)} 0000 0000 0000 0000 ${faker.datatype.number(min: 1111, max: 9999)}',
+                  title: 'Tytuł przelewu $index',
+                  transferId: index,
+                  accountingDate: DateTime.now(),
+                  accountHolderName:
+                      '${faker.locale.name.firstName} ${faker.locale.name.lastName}',
+                  bankId: faker.datatype.number(min: 0, max: 2),
+                )));
   }
 }
--- lib/injection.dart
+++ lib/injection.dart
@@ -1,11 +1,15 @@
 import 'package:flutter_playground/application/pages/login/cubit/bank_login_cubit.dart';
 import 'package:flutter_playground/data/bank/datasources/bank_data_source.dart';
 import 'package:flutter_playground/data/bank/repositiories/bank_account_repository_impl.dart';
+import 'package:flutter_playground/data/bank/repositiories/bank_transfer_repository_impl.dart';
 import 'package:flutter_playground/domain/bank/repositories/bank_account_repository.dart';
 import 'package:flutter_playground/domain/bank/usecases/get_bank_accounts_use_case.dart';
+import 'package:flutter_playground/domain/bank_transfer/repositories/bank_transfer_repository.dart';
+import 'package:flutter_playground/domain/bank_transfer/usecases/get_transfers_use_case.dart';
 import 'package:get_it/get_it.dart';
 
 import 'application/pages/bank/cubit/bank_cubit.dart';
+import 'data/bank/datasources/bank_transfer_data_source.dart';
 
 final sl = GetIt.instance;
 
@@ -19,13 +23,16 @@ Future<void> init() async {
 //
 // ! Use Cases
   sl.registerFactory(() => GetBankAccountsUseCase(sl()));
+  sl.registerFactory(() => GetBankTransfersUseCase(sl()));
 
 // ! Repositories
   sl.registerFactory<BankAccountRepository>(
       () => BankAccountRepositoryImpl(bankAccountDataSource: sl()));
 
+  sl.registerFactory<BankTransferRepository>(
+      () => BankTransferRepositoryImpl(bankTransferDataSource: sl()));
 // ! Data Layer
   sl.registerFactory<BankAccountDataSource>(() => BankAccountDataSourceImpl());
-
-
+  sl.registerFactory<BankTransferDataSource>(
+      () => BankTransferDataSourceImpl());
 }
