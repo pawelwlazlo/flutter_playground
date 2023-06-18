311202de68ed772c5705692810d4b3facafe0ee8
pawelwlazlo <pawel@wlazlo.dev>
2023-06-18 06:33:20 +0200

    usunięcie zbędnej klasy modelu

--- lib/application/pages/login/cubit/bank_login_cubit.dart
+++ lib/application/pages/login/cubit/bank_login_cubit.dart
@@ -1,7 +1,5 @@
 import 'package:equatable/equatable.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_playground/application/pages/login/cubit/bank_login_model.dart';
 
 part 'bank_login_state.dart';
 
