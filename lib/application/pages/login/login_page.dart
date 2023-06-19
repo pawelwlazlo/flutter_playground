8e2391fbcd7019b0bd631614f1b0b8b17b8b0dcb
pawelwlazlo <pawel@wlazlo.dev>
2023-06-19 11:51:38 +0200

    dodanie usecases dla użytkownika

--- lib/application/pages/login/login_page.dart
+++ lib/application/pages/login/login_page.dart
@@ -32,13 +32,11 @@ class BankLoginWidget extends StatelessWidget {
             ),
           );
         } else if (status == BankLoginStateEnum.pinSuccess) {
-
           Provider.of<BankCubit>(context, listen: false).logIn(
             userId: state.id!,
             login: state.login!,
             fullName: state.fullName!,
           );
-
 
           Navigator.pushReplacement(
             context,
@@ -49,14 +47,24 @@ class BankLoginWidget extends StatelessWidget {
       builder: (context, state) {
         final status = state.status;
 
+        final Widget activeWidget;
         if (status == BankLoginStateEnum.initial ||
             status == BankLoginStateEnum.error) {
-          return LoginWidget(state: state, theme: theme);
-        } else if (status == BankLoginStateEnum.success) {
-          return PinWidget(state: state, theme: theme);
-        } else if (status == BankLoginStateEnum.pinError) {
-          return PinWidget(state: state, theme: theme);
+          activeWidget = LoginWidget(state: state, theme: theme);
+        } else if (status == BankLoginStateEnum.success ||
+            status == BankLoginStateEnum.pinError) {
+          activeWidget = PinWidget(state: state, theme: theme);
+        } else {
+          activeWidget = const SizedBox();
         }
+
+        return Scaffold(
+            resizeToAvoidBottomInset: false,
+            backgroundColor: theme.scaffoldBackgroundColor,
+            appBar: const CustomAppBar(
+              title: 'Easy Bank Login',
+            ),
+            body: activeWidget);
         return const SizedBox();
       },
     );
@@ -74,68 +82,61 @@ class LoginWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      backgroundColor: theme.scaffoldBackgroundColor,
-      appBar: const CustomAppBar(
-        title: 'Easy Bank Login',
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(
-              child: Center(
-                child: SvgPicture.asset(
-                  'assets/eb2.svg',
-                  fit: BoxFit.fitWidth,
-                  width: 1200,
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Center(
+              child: SvgPicture.asset(
+                'assets/eb2.svg',
+                fit: BoxFit.fitWidth,
+                width: 1200,
+              ),
+            ),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                TextField(
+                  controller: _loginController,
+                  decoration: const InputDecoration(
+                    labelText: 'Login',
+                  ),
                 ),
-              ),
-            ),
-            Expanded(
-              child: Column(
-                children: [
-                  TextField(
-                    controller: _loginController,
-                    decoration: const InputDecoration(
-                      labelText: 'Login',
-                    ),
+                TextField(
+                  controller: _passwordController,
+                  decoration: const InputDecoration(
+                    labelText: 'Hasło',
                   ),
-                  TextField(
-                    controller: _passwordController,
-                    decoration: const InputDecoration(
-                      labelText: 'Hasło',
-                    ),
-                    obscureText: true,
-                  ),
-                ],
-              ),
+                  obscureText: true,
+                ),
+              ],
             ),
-            Expanded(
-              child: Column(
-                children: [
-                  const SizedBox(height: 16.0),
-                  ElevatedButton(
-                    onPressed: () {
-                      final String login = _loginController.text;
-                      final String password = _passwordController.text;
-                      context.read<BankLoginCubit>().logIn(login, password);
-                    },
-                    style: ElevatedButton.styleFrom(
-                      shape: CircleBorder(
-                          side: BorderSide(
-                              color: theme.colorScheme.primary, width: 4)),
-                      padding: const EdgeInsets.all(54),
-                    ),
-                    child: const Text('Zaloguj'),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {
+                    final String login = _loginController.text;
+                    final String password = _passwordController.text;
+                    context.read<BankLoginCubit>().logIn(login, password);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(
+                        side: BorderSide(
+                            color: theme.colorScheme.primary, width: 4)),
+                    padding: const EdgeInsets.all(54),
                   ),
-                ],
-              ),
+                  child: const Text('Zaloguj'),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
@@ -150,55 +151,49 @@ class PinWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final TextEditingController pinController = TextEditingController();
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: const CustomAppBar(
-        title: 'Easy Bank Login',
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Expanded(child: Text("Witaj ${state.fullName}")),
-            Expanded(
-              child: Column(
-                children: [
-                  TextField(
-                    controller: pinController,
-                    decoration: InputDecoration(
-                      labelText: 'PIN',
-                      errorText: state.status == BankLoginStateEnum.pinError
-                          ? 'Nieprawidłowy PIN'
-                          : null,
-                    ),
-                    obscureText: true,
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(child: Text("Witaj ${state.fullName}")),
+          Expanded(
+            child: Column(
+              children: [
+                TextField(
+                  controller: pinController,
+                  decoration: InputDecoration(
+                    labelText: 'PIN',
+                    errorText: state.status == BankLoginStateEnum.pinError
+                        ? 'Nieprawidłowy PIN'
+                        : null,
                   ),
-                ],
-              ),
+                  obscureText: true,
+                ),
+              ],
             ),
-            Expanded(
-              child: Column(
-                children: [
-                  const SizedBox(height: 16.0),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      shape: CircleBorder(
-                          side: BorderSide(
-                              color: theme.colorScheme.primary, width: 4)),
-                      padding: const EdgeInsets.all(54),
-                    ),
-                    onPressed: () {
-                      final String pin = pinController.text;
-                      context.read<BankLoginCubit>().submitPin(pin);
-                    },
-                    child: const Text('Zaloguj'),
+          ),
+          Expanded(
+            child: Column(
+              children: [
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(
+                        side: BorderSide(
+                            color: theme.colorScheme.primary, width: 4)),
+                    padding: const EdgeInsets.all(54),
                   ),
-                ],
-              ),
+                  onPressed: () {
+                    final String pin = pinController.text;
+                    context.read<BankLoginCubit>().submitPin(pin);
+                  },
+                  child: const Text('Zaloguj'),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
