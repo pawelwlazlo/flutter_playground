8f211f8a7088973ac49c51b6d8e92837122ed28f
pawelwlazlo <pawel@wlazlo.dev>
2023-06-19 11:58:34 +0200

    dodanie di

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,15 +1,17 @@
-import 'package:flutter_playground/application/pages/bank_transfer/cubit/bank_transfer_cubit.dart';
-import 'package:flutter_playground/application/pages/login/cubit/bank_login_cubit.dart';
 import 'package:flutter_playground/data/bank/datasources/bank_data_source.dart';
+import 'package:flutter_playground/data/bank/datasources/user_data_source.dart';
 import 'package:flutter_playground/data/bank/repositiories/bank_account_repository_impl.dart';
 import 'package:flutter_playground/data/bank/repositiories/bank_transfer_repository_impl.dart';
+import 'package:flutter_playground/data/bank/repositiories/user_repository_impl.dart';
 import 'package:flutter_playground/domain/bank/repositories/bank_account_repository.dart';
 import 'package:flutter_playground/domain/bank/usecases/get_bank_accounts_use_case.dart';
 import 'package:flutter_playground/domain/bank_transfer/repositories/bank_transfer_repository.dart';
 import 'package:flutter_playground/domain/bank_transfer/usecases/get_transfers_use_case.dart';
+import 'package:flutter_playground/domain/login/repositories/user_repository.dart';
+import 'package:flutter_playground/domain/login/usecases/check_user_pin_use_case.dart';
+import 'package:flutter_playground/domain/login/usecases/get_user_use_case.dart';
 import 'package:get_it/get_it.dart';
 
-import 'application/pages/bank/cubit/bank_cubit.dart';
 import 'data/bank/datasources/bank_transfer_data_source.dart';
 
 final sl = GetIt.instance;
@@ -28,6 +30,8 @@ Future<void> init() async {
 // ! Use Cases
   sl.registerFactory(() => GetBankAccountsUseCase(sl()));
   sl.registerFactory(() => GetBankTransfersUseCase(sl()));
+  sl.registerFactory(() => GetUserUseCase(sl()));
+  sl.registerFactory(() => CheckUserPinUseCase());
 
 // ! Repositories
   sl.registerFactory<BankAccountRepository>(
@@ -35,8 +39,12 @@ Future<void> init() async {
 
   sl.registerFactory<BankTransferRepository>(
       () => BankTransferRepositoryImpl(bankTransferDataSource: sl()));
+
+  sl.registerFactory<UserRepository>(() => UserRepositoryImpl(sl()));
+
 // ! Data Layer
   sl.registerFactory<BankAccountDataSource>(() => BankAccountDataSourceImpl());
   sl.registerFactory<BankTransferDataSource>(
       () => BankTransferDataSourceImpl());
+  sl.registerFactory<UserDataSource>(() => UserDataSourceImpl());
 }
